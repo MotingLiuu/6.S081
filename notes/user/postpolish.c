0c8dc7fc9fc76e45eca7b374bb6d665ceed0c094
MotingLiuu <moting.liuu@gmail.com>
2026-07-03 14:34:47 +0800

    added stack

--- notes/user/postpolish.c
+++ notes/user/postpolish.c
@@ -6,9 +6,37 @@
 #define MAXLEN 100
 
 char *whitespace = " \t\n";
-char *level1ops = "+-";
+char *level3ops = "+-";
 char *level2ops = "*/%";
 char *ops = "+-*/%()";
+
+typedef struct token {
+    char tokentype;
+    char *ps;
+    char *pe;
+} token;
+
+token tstack[MAXLEN];
+int top = 0;
+void push(char tokentype, char *ps, char *pe) {
+    if (top >= MAXLEN) {
+        printf("error: stack overflow\n");
+        exit(1);
+    }
+    tstack[top].tokentype = tokentype;
+    tstack[top].ps = ps;
+    tstack[top].pe = pe;
+    top++;
+}
+token pop() {
+    if (top <= 0) {
+        printf("error: stack underflow\n");
+        exit(1);
+    }
+    top--;
+    return tstack[top];
+}
+
 
 int postpolish(char *s, int level);
 int gettoken(char **s, char **ps, char **pe);
@@ -16,11 +44,18 @@ int gettoken(char **s, char **ps, char **pe);
 int test_gettoken();
 
 int main(int argc, char *argv[]) {
-    test_gettoken();
+    test_gettoken(); 
     return 0;
 }
 
 int test_gettoken() {
+    char *stack0 = "qwe";
+    char tokentype0 = 'v';
+    push(tokentype0, stack0, stack0 + strlen(stack0));
+    char tokentype1 = '*';
+    push(tokentype1, 0, 0);
+    char tokentype2 = '+';
+    push(tokentype2, 0, 0);
     char *test_str1 = "abc + def + (ghi * jkl) + mno";
     printf("test_str1: %s\n", test_str1);
     char **psc = &test_str1;
@@ -45,6 +80,12 @@ int test_gettoken() {
 }
 
 int gettoken(char **psc, char **ps, char **pe) {
+    if (top != 0) {
+        token t = pop();
+        *ps = t.ps;
+        *pe = t.pe;
+        return t.tokentype;
+    }
     while (**psc && strchr(whitespace, **psc))
         (*psc)++;
     if (**psc == '\0')
@@ -66,17 +107,23 @@ int gettoken(char **psc, char **ps, char **pe) {
             *pe = *psc;
             return 'v';
     }
+    return -1;
 }
 
-int postpolish(char *s, int level) {
-    char **pc = &s;
-    int tokentype;
-    char **ps, **pe;
-    while ((tokentype = gettoken(&s, ps, pe)) != 0) {
-        switch (tokentype) {
-            ;
-        }
-    }
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

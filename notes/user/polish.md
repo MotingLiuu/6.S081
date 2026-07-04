41ffd20b8909cf97af13530ea40007c37e85ca55
MotingLiuu <moting.liuu@gmail.com>
2026-07-04 18:53:35 +0800

    completed polish with stack

--- notes/user/polish.md
+++ notes/user/polish.md
@@ -1,14 +1,48 @@
 # polish
 
-## just + - * /
+Expressions
+1. unit expression `var`, `()`, with highest priority.
+2. expression level2 with `*/%`, `unit op unit op ...`, with second priority.
+3. expression level3 with `+-`, `exlevel2 op exlevel2 op ...`, with third priority.
 
-e.g.
-$1 + (2 * 3) - (4 * 5) + 10 $
+## Convert to postfix
+Consider expression with level3, `ex<=level2 op ex<=level2 op ...`
+1. deal with `ex <= level2` first
+2. deal with second `op`, then print `op`.
+3. ...continue
 
-begin:
-print `1`
-push `+` into stack, find the right hand of `+`. Right hand of `+` is `var`or `var * var / var ...`
-just parse the right hand of '+' after that pop and print `+`
+Without recursion:
+
+stack vars
+stack ops
+
+while:
+    if '(', create a new frame (push '(' to stack, treat everything after '(' as a new stack.
+    if 'v', just print. it is a ex just contain itself.
+    if `op`: there are two cases:
+        1. if current frame is empty, just push `op` to stack(create a new frame to compute right ex). 
+        2. if current frame is not empty,
+            1. if frame top's op's priority is higher than 'op', then pop 'op' from stack(end current frame), and push it to current frame(create a new frame to compute right ex).
+            2. if frame top's op's priority is lower than 'op', push 'op' to stack(create a new frmae to compute right ex).
+    if ')', pop frames from stack untile find '('.
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- notes/user/postpolish.c
+++ notes/user/postpolish.c
@@ -10,6 +10,14 @@ char *level3ops = "+-";
 char *level2ops = "*/%";
 char *ops = "+-*/%()";
 
+int oplevel(char op) {
+    if (strchr(level3ops, op))
+        return 3;
+    if (strchr(level2ops, op))
+        return 2;
+    return 1;
+}
+
 typedef struct token {
     char tokentype;
     char *ps;
@@ -28,6 +36,7 @@ void push(char tokentype, char *ps, char *pe) {
     tstack[top].pe = pe;
     top++;
 }
+
 token pop() {
     if (top <= 0) {
         printf("error: stack underflow\n");
@@ -37,8 +46,13 @@ token pop() {
     return tstack[top];
 }
 
+void clearstack() {
+    top = 0;
+}
+
 
 int parse(char **s, int level);
+int parse_stack(char **s);
 int gettoken(char **s, char **ps, char **pe);
 
 int test_gettoken();
@@ -82,9 +96,16 @@ int test_gettoken() {
 }
 
 int test_parse() {
-    char *test_str1 = "a * ((b - c) / (d + e))";
-    printf("test_str1: %s\n", test_str1);
+    char *test_str1 = "a * ((b - c) / (d + e)) + ((((q) * (w - r))))";
+    char *test_str2 = "a * ((b - c) / (d + e)) + ((((q) * (w - r))))";
+    printf("test_str: %s\n", test_str1);
+    printf("result of parse: ");
     parse(&test_str1, 3);
+    clearstack();
+    printf("\n");
+    printf("result of parse_stack: ");
+    parse_stack(&test_str2);
+    printf("\n");
     return 0;
 }
 
@@ -179,6 +200,71 @@ int parse(char **s, int level) {
         }
     return 0;
 }
+
+int parse_stack(char **s) {
+    char *ps, *pe;
+    int tokentype;
+    char ops[MAXLEN];
+    int opsp = 0;
+    // test str is a * ((b -c) / (d + e))
+    while (1) {
+        tokentype = gettoken(s, &ps, &pe);
+
+        if (tokentype == 0) {
+            while (opsp > 0) {
+                printf("%c ", ops[--opsp]);
+            }
+            break;
+        }
+        if (tokentype == -1) {
+            printf("error: tokentype is -1\n");
+        }
+
+        switch (tokentype) {
+            case '(':
+                if (opsp >= MAXLEN) {
+                    printf("error: stack overflow\n");
+                    return -1;
+                }
+                ops[opsp++] = tokentype;
+                break;
+            case 'v':
+                while (ps < pe) {
+                    putchar(*ps);
+                    (ps)++;
+                }
+                printf(" ");
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+                if (opsp != 0 && ops[opsp - 1] != '(' && oplevel(ops[opsp - 1]) <= oplevel(tokentype)) {
+                    printf("%c ",ops[--opsp]);
+                }
+                ops[opsp++] = tokentype;
+                break;
+            case ')':
+                while (opsp > 0 && ops[opsp - 1] != '(') {
+                    printf("%c ",ops[--opsp]);
+                }
+                opsp--;
+                break;
+        }
+
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 
 
 
